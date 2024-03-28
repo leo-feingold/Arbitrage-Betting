c732bb4b02f2450a4d7930c61692521a21caf89f
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-03-28 13:47:37 -0400

    placed bets

--- pickAnalysis.py
+++ pickAnalysis.py
@@ -6,6 +6,9 @@ from collections import Counter
 
 
 # load the data and create a new column called absDiff that is the absolute value of the difference column 
+# for the actual placed bets: (kinda ass) 
+#df = pd.read_csv('/Users/leofeingold/Documents/GitHub/Arbitrage-Betting/bets.csv').rename(columns = lambda x: x.lower())
+# other bets using both mgm and fanduel from preseason (not last day of the preseason)
 df = pd.read_csv('/Users/leofeingold/Documents/GitHub/Arbitrage-Betting/fullData.csv').rename(columns = lambda x: x.lower())
 df = df.assign(
     absDiff = lambda x: abs(x.difference)
@@ -15,7 +18,7 @@ df = df.assign(
 df['expected value (%)'] = pd.to_numeric(df['expected value (%)'], errors='coerce')
 
 # Restrict the bets down to only bets with at least an 8% ROI (8 is somewhat arbitralily chosen...)
-minEV = 8
+minEV = 11 #THIS IS SUPER IMPORTANT ODD NUMBER OF BETS IS WAY BETTER THAN EVEN!!!
 mask = df['expected value (%)'] >= minEV
 df = df[mask]
 
@@ -33,18 +36,19 @@ def addCols(df, betAmount):
     return df
 
 # create the dataframe with those columns
-moneyPerBet = 100
+moneyPerBet = 2000/len(df)
 df = addCols(df, moneyPerBet)
 
-
+numSims = 20000
 # here is the random sample and simulation:
 def randomSample(df):
     totalWins = 0
     totalLosses = 0
-    distribution = []
+    bet_distribution = []
+    profit_distribution = []
     totalMoney = 0
 
-    for sample in range(1000): # 1000 seasons of simulations 
+    for sample in range(numSims): # numSims seasons of simulations 
         winCount = 0
         loseCount = 0
         money = 0
@@ -57,27 +61,34 @@ def randomSample(df):
                 loseCount += 1
                 money += df.loss.iloc[i]
 
-        distribution.append(winCount)
+        bet_distribution.append(winCount)
+        profit_distribution.append(money)
 
-        print(f"Num Wins: {winCount}, Num Losses: {loseCount}, Result: {winCount/(winCount + loseCount)}, Money: {money}")
+        #print(f"Num Wins: {winCount}, Num Losses: {loseCount}, Result: {winCount/(winCount + loseCount)}, Money: {money}")
         totalMoney += money
         totalWins += winCount
         totalLosses += loseCount
 
-    win_counts = Counter(distribution)
-    result = totalWins / (totalWins + totalLosses)
-    resultStr = f"Num Wins: {totalWins}, Num Losses: {totalLosses}, Result: {result}, Average Money (per year): {totalMoney/1000}"
+    win_counts = Counter(bet_distribution)
 
-    prob_distribution = {k: v / 1000 for k, v in win_counts.items()}
+    result = totalWins / (totalWins + totalLosses)
+    resultStr = f"Num Wins: {totalWins}, Num Losses: {totalLosses}, Result: {result}, Average Money Per Bet (per year): {(totalMoney/numSims)/(len(df))}"
+    print(resultStr)
+
+    prob_distribution = {k: v / numSims for k, v in win_counts.items()}
 
     # Plot the probability distribution
     plt.bar(prob_distribution.keys(), prob_distribution.values())
     plt.xlabel('Winning Bets (per season)')
     plt.ylabel('Probability of Occurance')
     plt.title('Probability Distribution of Win Counts')
-    plt.suptitle(f"Dollars Per Bet: {moneyPerBet}, Simulated Seasons: 1000, Average Profit: ${totalMoney/1000:.2f}, Min ROI: {minEV}%, Number of Bets: {len(df)}")
+    plt.suptitle(f"Dollars Per Bet: {moneyPerBet}, Simulated Seasons: {numSims}, Average Profit: ${totalMoney/numSims:.2f}, Min ROI: {minEV}%, Number of Bets: {len(df)}")
+
+    for x, y in zip(prob_distribution.keys(), prob_distribution.values()):
+        plt.text(x, y, f'{y:.2f}', ha='center', va='bottom')
+
     plt.show()
 
-    return resultStr
-
 print(randomSample(df))
+
+
